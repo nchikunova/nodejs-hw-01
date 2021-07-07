5ec07d120b924d62179a124bc9756b75cc45e4c1
Nataliya <nchikunova86@gmail.com>
2021-07-07 15:12:53 +0300

    init hw nodejs

---
+++ README.md
@@ -0,0 +1,24 @@
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+
+node index.js --action list
+
+https://ibb.co/Qc6L2dL
+
+# Получаем контакт по id
+
+node index.js --action get --id 5
+
+https://ibb.co/Fb522bc
+
+# Добавляем контакт
+
+node index.js --action add --name Mango --email mango@gmail.com --phone
+322-22-22
+
+https://ibb.co/kDP7kSc
+
+# Удаляем контакт
+
+node index.js --action remove --id=3
+
+https://ibb.co/vdFpqx2
---
+++ contacts.js
@@ -0,0 +1,101 @@
+const fs = require('fs').promises
+const path = require('path')
+const contactsPath = path.join(__dirname, './db/contacts.json')
+const { v4: uuidv4 } = require('uuid')
+
+async function parsedContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf8')
+    return JSON.parse(data)
+  } catch (error) {
+    return console.error(error.message)
+  }
+}
+
+async function listContacts() {
+  try {
+    const contacts = await parsedContacts()
+    console.table(contacts)
+    return contacts
+  } catch (error) {
+    return console.error(error.message)
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await parsedContacts()
+    const contact = contacts.find(({ id }) => id === contactId)
+    if (!contact) return console.error(`Contact Id ${contactId} is not found`)
+
+    console.log(`Contact Id ${contactId}`)
+    console.table(contact)
+    return contact
+  } catch (error) {
+    return console.error(error.message)
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await parsedContacts()
+    const updatedContacts = contacts.filter(({ id }) => id !== contactId)
+
+    if (contacts.length === updatedContacts.length) {
+      return console.error(`Contact Id ${contactId} is not found`)
+    }
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(updatedContacts, null, 2),
+      'utf8',
+    )
+    console.log('Contact deleted!')
+    console.table(updatedContacts)
+
+    return updatedContacts
+  } catch (error) {
+    return console.error(error.message)
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await parsedContacts()
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase(),
+      )
+    ) {
+      return console.warn('This name is already exists!')
+    }
+
+    if (contacts.find(contact => contact.email === email)) {
+      return console.warn('This email is already exists!')
+    }
+    if (contacts.find(contact => contact.phone === phone)) {
+      return console.warn('This phone is already exists!')
+    }
+
+    const newContact = { id: uuidv4(), name, email, phone }
+    const newContacts = [...contacts, newContact]
+
+    await fs.writeFile(
+      contactsPath,
+      JSON.stringify(newContacts, null, 2),
+      'utf-8',
+    )
+    console.log('Added contact')
+    console.table(newContacts)
+
+    return newContacts
+  } catch (error) {
+    return console.error(error.message)
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+}
